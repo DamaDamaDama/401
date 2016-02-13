1bbe3da0b0cf9186f6c562b3a8d132357257f517
DamaDamaDama <AEM108@pitt.edu>
2016-02-13 03:34:51 -0500

    File system in place, can't create files for some reason can only load

--- Blackjack.java
+++ Blackjack.java
@@ -2,18 +2,95 @@ import java.io.*;
 import java.util.*;
 
 public class Blackjack {
-	public static void main(String[] args){ //*******what i'm working on so far: file system + check movie class from 0007 to see how objects and classes work in a real example
-		Scanner name = new Scanner(System.in);
-		String usersName = "";
-		double usersMoney = 0;
+	public static void main(String[] args) throws IOException { //*******what i'm working on so far: file system + check movie class from 0007 to see how objects and classes work in a real example
+		Scanner usersName = new Scanner(System.in); //what is meant by how random generated cards are stored?
+		Scanner yn = new Scanner(System.in);
+		Scanner betamt = new Scanner(System.in);
+		String responsePlay = "";
+		String name = "";
+		String responseBet = "";
+		double responseBetConverted; //Jan 13th: most recent problem is getting it to make a new file if I didnt have a Blerg.txt to begin with etc
+		double money = 100.00;
 		int handsPlayed = 0;
 		int handsWon = 0;
+		boolean newP = true;
+		boolean r2p = true;
+		boolean bet = true;
+		boolean goodBet = false;
+		Random rnd = new Random();
 		
-		Player user = new Player(usersName, usersMoney, handsPlayed, handsWon);
+		System.out.println("I N F I N I T E - B L A C K J A C K  ©2016");
 		
-		System.out.println("Enter your name to begin!");
-		usersName = name.nextLine();
-		
-		
+		while(newP){
+			System.out.println("Enter your name to begin!");
+			name = usersName.nextLine();
+			Player user = new Player(name, money, handsPlayed, handsWon);
+			
+			if(isAlphabet(name)){
+				newP = false; //after this line I probably want another if/try where it checks for an old user
+				System.out.println("Thanks!");
+				if(!Player.f.exists()){  
+					Player.writePlayer(user);
+				}
+				Player.printPlayer(user);
+				while(r2p){
+					System.out.println("Ready to play? Enter Y/N");
+					responsePlay = yn.nextLine();
+					if(responsePlay.equals("Y") || responsePlay.equals("y")){
+						r2p = false;
+						while(bet){
+							System.out.println("How much will you be betting?");
+							responseBet = betamt.nextLine();
+							if(isNumber(responseBet)){
+								bet = false;
+								responseBetConverted = Double.parseDouble(responseBet);
+								Player.money = (money - responseBetConverted);
+								Player.printPlayer(user);
+							}
+							else{
+								System.out.println("That's not an appropriate monetary amount!");
+							}
+						}
+							
+					}
+					else if(responsePlay.equals("N") || responsePlay.equals("n")){
+						System.out.println("See you next time!");
+						System.exit(0);
+					}
+				}
+			}
+			else{
+				System.out.println("Enter an actual name please...");
+			}
+			
+		}
+	}
+	
+	public static boolean isAlphabet(String name) { //make sure a string is only letters
+	    char[] chars = name.toCharArray();
+
+	    for (char c : chars) {
+	        if(!Character.isLetter(c) && !Character.isWhitespace(c)) { //modified to also allow spaces
+	            return false;
+	        }
+	    }
+
+	    return true;
+	}
+	
+	public static boolean isNumber(String name) { //make sure a string is only numbers
+		try{
+	    	Double test;
+	    	test = Double.parseDouble(name);
+	    	if(test > 0){
+	    		return true;
+	    	}
+	    	else{
+	    		return false;
+	    	}    	
+	    }
+	    catch (NumberFormatException e){ //try catch will toss any garbage input out and only accept above 0 with or without decimals
+	    	return false;
+	    }
 	}
 }
--- Player.java
+++ Player.java
@@ -3,11 +3,12 @@ import java.util.*;
 
 public class Player {
 	
-	private String name;
-	private double money;
-	private int handsPlayed;
-	private int handsWon;
-
+	static String name;
+	static double money;
+	static int handsPlayed;
+	static int handsWon;
+	static File f = new File("C:\\Users\\Alan\\GitHub\\401\\" + name + ".txt");
+	
 	public Player(String playerName, double playerMoney, int playerHandsPlayed, int playerHandsWon){
 		name = playerName;
 		money = playerMoney;
@@ -15,4 +16,49 @@ public class Player {
 		handsWon = playerHandsWon;
 	}
 	
+	public Player(){
+		
+	}
+	
+	public static void setName(String n){
+		name = n;
+	}
+	
+	public static void setMoney(double m){
+		money = m;
+	}
+	
+	public static void setPlayed(int p){
+		handsPlayed = p;
+	}
+	
+	public static void setWon(int w){
+		handsWon = w;
+	}
+	
+	public static void printPlayer(Player user) throws FileNotFoundException{
+		f = new File("C:\\Users\\Alan\\GitHub\\401\\" + name + ".txt");
+		Scanner read = new Scanner(f);
+		String profile = "";
+		
+		System.out.println("Your player information, " + name + ":");
+		
+		for (int i = 0; i < 4; i++){
+			profile = read.nextLine();
+			System.out.println(profile);
+		}
+		read.close();
+		
+	}
+	public static void writePlayer (Player user) throws IOException{
+		f = new File("C:\\Users\\Alan\\GitHub\\401\\" + name + ".txt"); //why is this not creating the file at that location?
+																		//i have to manually make a Blerg.txt etc so far
+		PrintWriter log = new PrintWriter("C:\\Users\\Alan\\GitHub\\401\\" + name + ".txt");
+		log.println("Name: " + name);
+		log.println("Total Funds: $" + money);
+		log.println("Hands Played: " + handsPlayed);
+		log.println("Hands Won: " + handsWon);
+		log.close();
+			
+	}
 }
