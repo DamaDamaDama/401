e458201ffbfbed61262d2eaf23e2dc314b5c03e4
DamaDamaDama <AEM108@pitt.edu>
2016-03-23 17:24:31 -0400

    added a3 mateirals etc
    qweqweq

---
+++ Assignments/testarrays.java
@@ -0,0 +1,14 @@
+package Assignments;
+
+public class testarrays {
+	public static void main(String[] args){
+		int[] fuck = new int[2];
+		fuck[0] = 1;
+		fuck[1] = 2;
+		fuck = new int[15];
+		fuck[7] = 7;
+		System.out.println(fuck[7]);
+		
+		//can I even resize an array or now becasue this shit works apparently
+	}
+}
