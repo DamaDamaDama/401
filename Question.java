e98da1328240a36629ac6b175c026a22821251b6
DamaDamaDama <aem108@pitt.edu>
2016-03-26 22:12:11 -0400

    ya

--- Question.java
+++ Question.java
@@ -1,42 +1,41 @@
-
 public class Question {
-	//"store users answers in a question object"
-	private static String questions;
-	private static String[] answers;
-	private int correctAnswers;
-	private int nAttempts;
-	private int nCorrect;
-	
-	public Question(String q, String[] ans, int cAns, int attempts, int correct){
-		questions = q;
-		answers = ans;
-		correctAnswers = cAns;
-		nAttempts = attempts;
-		nCorrect = correct;
-		
-	}
-	
-	public Question(){
-		
-	}
-	
-	public static String printQuestion(Question q){
-		
-		return questions;
-	}
-	
-	public static void printAnswers(Question q){
-		
-		for(int i = 0; i < answers.length; i++){
-			System.out.println(i + ": " + answers[i]);
-		}
-	}
-	
-	public static int nAnswers(Question q){
-		int increment = 0;
-		for(int i = 0; i < answers.length - 1; i++){
-			increment++;
-		}
-		return increment;
-	}
+    //"store users answers in a question object"
+    private String questions; //removed static
+    private String[] answers; //removed static
+    private int correctAnswers;
+    private int nAttempts;
+    private int nCorrect;
+ 
+    public Question(String q, String[] ans, int cAns, int attempts, int correct){
+        questions = q;
+        answers = ans;
+        correctAnswers = cAns;
+        nAttempts = attempts;
+        nCorrect = correct;
+ 
+    }
+ 
+    public Question(){
+ 
+    }
+ 
+    public String printQuestion(){ //Removed the Question q parameter
+        return questions;
+        //return questions;
+    }
+ 
+    public void printAnswers(){ //Removed the Question q parameter
+ 
+        for(int i = 0; i < answers.length; i++){
+            System.out.println(i + ": " + answers[i]);
+        }
+    }
+ 
+    public int nAnswers(){ //Removed the Question q parameter
+        int increment = 0;
+        for(int i = 0; i < answers.length - 1; i++){
+            increment++;
+        }
+        return increment;
+    }
 }
--- Quiz.java
+++ Quiz.java
@@ -1,129 +1,128 @@
 import java.io.*;
 import java.util.*;
 import Assignments.Player;
-
+ 
 public class Quiz {
-	static String quiz = "quiz.txt";
-	static File f = new File(quiz);
-	static ArrayList<Question> questions = new ArrayList<Question>(4);
-	
-	public static void main(String[] args) throws IOException{
-		args[0] = "quiz.txt";
-		Scanner answer = new Scanner(System.in);
-		String userAnswer = "";
-		int userAnswerConv = 0;
-		String[] qAns = new String[3];
-		int qCounter = 0;
-		boolean testing = true;
-		boolean sameProb = true;
-		boolean results = true;
-		
-		////////////////////////////////////////////////////////////////
-		
-		System.out.println();
-		System.out.println("Welcome to Quiz by Alan Munirji");
-		System.out.println();
-		
-		readQuiz();
-		printQuiz();
-		
-		while(testing){ //this section of code is for taking the quiz
-			sameProb = true;
-			if(qCounter < questions.size()){ 
-				System.out.println("Question #" + qCounter);
-				System.out.println(Question.printQuestion(questions.get(qCounter)));
-			}
-			else{
-				testing = false;
-				System.out.println("That concludes all of the questions.");
-				break;
-			}
-			System.out.println("Answers: ");
-			Question.printAnswers(questions.get(qCounter));
-			while(sameProb){
-				userAnswer = answer.nextLine();
-				try{
-					userAnswerConv = Integer.parseInt(userAnswer);
-					if(userAnswerConv < 0 || userAnswerConv > Question.nAnswers(questions.get(qCounter))){
-						System.out.println("Inappropriate response. Please enter an appropriate number");
-					}
-					else{
-						System.out.println("Your response has been recorded.");
-						qCounter++;
-						sameProb = false;
-					}
-				}
-				catch(NumberFormatException NFE){
-					System.out.println("Inappropriate response. Please enter an appropriate number");
-				}
-			}
-		}
-		while(results){ //this section of code is for displaying the results back
-			break;
-		}
-		
-	}
-	
-	public static void updateQuiz() throws IOException{ //enter stuff into a quiz file
-		f.createNewFile();
-	
-		PrintWriter log = new PrintWriter("quiz.txt");
-		log.println("test");
-		log.println();
-		log.close();
-			
-	}
-	
-	public static void printQuiz() throws FileNotFoundException{ //print quiz file out
-		f = new File("quiz.txt");
-		Scanner read = new Scanner(f);
-		boolean more = true;
-		
-		System.out.println("Quiz File Contents:");
-			
-		while(more){
-			try{
-				System.out.println(read.nextLine());
-			}
-			catch(Exception e){
-				more = false;
-			}
-		
-		}
-		
-		read.close();
-		
-	} 
-	
-	public static void readQuiz() throws FileNotFoundException{ //read whats on the file, we want to insert it into question objects
-		f = new File("quiz.txt");
-		Scanner read = new Scanner(f);
-		String line;
-		String question;
-		int nAns = 0;
-		int cAns = 0;
-		int attempts = 0;
-		int nCorrect = 0;
-		
-		while(read.hasNextLine()){
-			//if(read.nextLine().contains("?")){
-				question = read.nextLine();
-				System.out.println("DEBUG ONLY Question being added: " + question);
-				nAns = Integer.parseInt(read.nextLine());
-				String[] ans = new String[nAns];
-				for(int i = 0; i < nAns; i++){
-					ans[i] = read.nextLine();
-				}
-				cAns = Integer.parseInt(read.nextLine());
-				attempts = Integer.parseInt(read.nextLine());
-				nCorrect = Integer.parseInt(read.nextLine());
-				
-				questions.add(new Question(question, ans, cAns, attempts, nCorrect));
-				System.out.println(Question.printQuestion(questions.get(0)));  //wtf??? its ONLY index 0
-
-			//}
-		}
-		read.close();
-		
-	} 
+    static String quiz = "quiz.txt";
+    static File f = new File(quiz);
+    static ArrayList<Question> questions = new ArrayList<Question>();
+ 
+    public static void main(String[] args) throws IOException{
+        args[0] = "quiz.txt";
+        Scanner answer = new Scanner(System.in);
+        String userAnswer = "";
+        int userAnswerConv = 0;
+        String[] qAns = new String[3];
+        int qCounter = 0;
+        boolean testing = true;
+        boolean sameProb = true;
+        boolean results = true;
+ 
+        ////////////////////////////////////////////////////////////////
+ 
+        System.out.println();
+        System.out.println("Welcome to Quiz by Alan Munirji");
+        System.out.println();
+ 
+        readQuiz();
+        printQuiz();
+ 
+        while(testing){ //this section of code is for taking the quiz
+            sameProb = true;
+            if(qCounter < questions.size()){
+                System.out.println("Question #" + qCounter);
+                System.out.println(questions.get(qCounter).printQuestion());
+            }
+            else{
+                testing = false;
+                System.out.println("That concludes all of the questions.");
+                break;
+            }
+            System.out.println("Answers: ");
+            questions.get(qCounter).printAnswers(); //modified from Question.printAnswers(questions.get(qCounter))
+            while(sameProb){
+                userAnswer = answer.nextLine();
+                try{
+                    userAnswerConv = Integer.parseInt(userAnswer);
+                    if(userAnswerConv < 0 || userAnswerConv > questions.get(qCounter).nAnswers()){ //changed Question.nAnswers(...) to questions.get(qCounter).nAnswers()
+                        System.out.println("Inappropriate response. Please enter an appropriate number");
+                    }
+                    else{
+                        System.out.println("Your response has been recorded.");
+                        qCounter++;
+                        sameProb = false;
+                    }
+                }
+                catch(NumberFormatException NFE){
+                    System.out.println("Inappropriate response. Please enter an appropriate number");
+                }
+            }
+        }
+        while(results){ //this section of code is for displaying the results back
+            break;
+        }
+ 
+    }
+ 
+    public static void updateQuiz() throws IOException{ //enter stuff into a quiz file
+        f.createNewFile();
+ 
+        PrintWriter log = new PrintWriter("quiz.txt");
+        log.println("test");
+        log.println();
+        log.close();
+ 
+    }
+ 
+    public static void printQuiz() throws FileNotFoundException{ //print quiz file out
+        f = new File("quiz.txt");
+        Scanner read = new Scanner(f);
+        boolean more = true;
+ 
+        System.out.println("Quiz File Contents:");
+ 
+        while(more){
+            try{
+                System.out.println(read.nextLine());
+            }
+            catch(Exception e){
+                more = false;
+            }
+ 
+        }
+ 
+        read.close();
+ 
+    }
+ 
+    public static void readQuiz() throws FileNotFoundException{ //read whats on the file, we want to insert it into question objects
+        f = new File("quiz.txt");
+        Scanner read = new Scanner(f);
+        String line;
+        String question;
+        int nAns = 0;
+        int cAns = 0;
+        int attempts = 0;
+        int nCorrect = 0;
+ 
+        while(read.hasNextLine()){
+            //if(read.nextLine().contains("?")){
+                question = read.nextLine();
+                nAns = Integer.parseInt(read.nextLine());
+                String[] ans = new String[nAns];
+                for(int i = 0; i < nAns; i++){
+                    ans[i] = read.nextLine();
+                }
+                cAns = Integer.parseInt(read.nextLine());
+                attempts = Integer.parseInt(read.nextLine());
+                nCorrect = Integer.parseInt(read.nextLine());
+ 
+                questions.add(new Question(question, ans, cAns, attempts, nCorrect));
+                
+ 
+            //}
+        }
+        read.close();
+ 
+    }
 }
