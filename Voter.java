41f2ef6afacb24d885fa43f8b0ee26c0b979fd74
DamaDamaDama <AEM108@pitt.edu>
2016-04-01 02:02:26 -0400

    created2

--- Voter.java
+++ Voter.java
@@ -2,10 +2,9 @@ import java.io.File;
 import java.io.IOException;
 
 public class Voter {
-	static String quiz = args[0];
-    static File f = new File(quiz);
-    
     public static void main(String[] args) throws IOException{
-    
+    	String quiz = args[0];
+        File f = new File(quiz);
+        
     }
 }
