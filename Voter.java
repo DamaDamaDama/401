ea4a3a470ab182b6fa5e954654dad93df126cf34
DamaDamaDama <AEM108@pitt.edu>
2016-04-05 01:15:53 -0400

    .

--- Voter.java
+++ Voter.java
@@ -7,11 +7,12 @@ public class Voter {
 	
 	private static final int HEIGHT = 600;
 	private static final int WIDTH = 800;
-	 
+	
     public static void main(String[] args) throws IOException{
     	//String voter = args[0]; //accept the text file that has voter information
         //File f = new File(voter);
         
+    	
         JFrame window = new JFrame("TEST JFrame");
     	window.setSize(WIDTH, HEIGHT);
     	
@@ -23,6 +24,8 @@ public class Voter {
     	msg.setFont(new Font("TimesRoman", Font.BOLD, 36));
     	msg.setForeground(Color.BLACK);
     	window.add(msg);
+    	
+    	JButton b = new JButton("TEST_BUTTON");
     	
     	window.setVisible(true);
     }
